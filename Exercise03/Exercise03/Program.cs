e361cb47a789d962b548fa84ad7d22531283d8d7
Yang Yu <yy4252gg@gmail.com>
2022-06-29 17:33:23 -0400

    C#Assignment 1 Practice
    C#Assignment 1 Practice

---
+++ Exercise03/Exercise03/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int max = 100;
+for (byte i = 0; i < max; i++)
+{
+    Console.WriteLine("Hello, World! {0}",i);
+}
+
+
+int correct = new Random().Next(3) + 1;
+Console.WriteLine("Please input your guess");
+
+int guess = int.Parse(Console.ReadLine());
+if(guess >= 1 && guess <= 3)
+{
+    if(guess == correct)
+    {
+        Console.WriteLine("Correct !! ");
+    }else if (guess > correct)
+    {
+        Console.WriteLine("The guess is high");
+    }
+    else
+    {
+        Console.WriteLine("The guess is low");
+    }
+}
+else
+{
+    Console.WriteLine("Out of Range");
+}
+
+Console.WriteLine("Correct is {0}", correct);
+
+
