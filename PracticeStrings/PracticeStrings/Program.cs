e7155d42083473359f7b1cb7dfcaae9fdc692364
Yang Yu <yy4252gg@gmail.com>
2022-06-29 22:11:18 -0400

    PracticeStrings C# Ass.2

---
+++ PracticeStrings/PracticeStrings/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.WriteLine("aaaabbc");
+revert("aaaabbc");
+
+Console.WriteLine("Input your word!");
+String input = Console.ReadLine();
+revert(input);
+static void revert(String s)
+{
+    StringBuilder sb = new StringBuilder("", 50);
+    for(int i = s.Length - 1; i >=0 ; i--)
+    {
+        sb.Append(s[i]);
+    }
+    Console.WriteLine(sb);
+}
