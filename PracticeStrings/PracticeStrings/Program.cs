52c62dff7844bb0d2f53387d044e32d266722d87
Yang Yu <yy4252gg@gmail.com>
2022-06-29 22:49:51 -0400

    parseURL

--- PracticeStrings/PracticeStrings/Program.cs
+++ PracticeStrings/PracticeStrings/Program.cs
@@ -3,6 +3,11 @@ using System.Text;
 
 Console.WriteLine("aaaabbc");
 revert("aaaabbc");
+Console.WriteLine("Input your URL!");
+String url = Console.ReadLine();
+ParseUrl(url);
+
+
 
 Console.WriteLine("Input your word!");
 String input = Console.ReadLine();
@@ -15,4 +20,27 @@ static void revert(String s)
         sb.Append(s[i]);
     }
     Console.WriteLine(sb);
+}
+
+static void ParseUrl(String s)
+{
+    int check = s.IndexOf("://");
+    //start = check + 3;
+    int sps = 0;
+    int end = s.Length - 1;
+    int e = s.LastIndexOf('/');
+    if(check + 2 != e)
+    {
+        end = e;
+    }
+    if (check + 3 < s.Length - 1)
+    {
+        Console.WriteLine(s.Substring(check + 3, end));
+    }
+    else
+    {
+        Console.WriteLine("Wrong input");
+    }
+    
+    
 }
