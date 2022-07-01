4e0fb47f1967dc587c7c223fbc605fa7e7849cbc
Yang Yu <yy4252gg@gmail.com>
2022-06-30 20:50:08 -0400

    student and instructor
    student and instructor

---
+++ C#Assignment3/Person/Person/person.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Person
+{
+	abstract class person1
+	{
+		public int height { get; set; }
+		//int id;
+
+		public abstract void DoWork();
+
+
+		public static void Main(string[] args)
+		{
+            var s = new student();
+            s.DoWork();
+
+
+            var i = new instructor();
+            i.DoWork();
+        }
+	}
+
+	 class student : person1
+    {
+		public override void DoWork()
+        {
+            Console.WriteLine("Student : Just study");
+        }
+	}
+
+	 class instructor : person1
+	{
+		public override void DoWork()
+		{
+			Console.WriteLine("Instructor : Teach students");
+		}
+	}
+}
+
