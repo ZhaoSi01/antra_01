4c9e69ee5611e4e00982d4d484fd029fd021eef2
Yang Yu <yy4252gg@gmail.com>
2022-06-30 22:27:02 -0400

    6th
    6th

---
+++ C#Assignment3/InterfacesPractice/InterfacesPractice/IStudent.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace InterfacesPractice
+{
+	public interface IStudent
+	{
+	}
+}
+
---
+++ C#Assignment3/InterfacesPractice/InterfacesPractice/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace InterfacesPractice
+{
+	public class Student
+	{
+		public Student()
+		{
+		}
+	}
+}
+
