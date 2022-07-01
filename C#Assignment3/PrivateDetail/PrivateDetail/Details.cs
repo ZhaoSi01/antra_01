5b94909edf108294a3b969a1123c4e0b66d108e7
Yang Yu <yy4252gg@gmail.com>
2022-06-30 21:07:42 -0400

    private details

---
+++ C#Assignment3/PrivateDetail/PrivateDetail/Details.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace PrivateDetail
+{
+	 class Details
+	{
+		private int id { get; set; }
+		private int height { get; set; }
+		private String name { get; set; }
+        //Console.WriteLine("Id: {0} + Height: {1} + Name: {2}", D, int height, String name);
+
+
+        public static void Main(string[] args)
+        {
+			var v = new Details();
+			v.id = 1;
+            Console.WriteLine("ID: "+ v.id);
+			v.height = 189;
+			v.name = "Y";
+			Console.WriteLine("Height: " + v.height);
+			Console.WriteLine("Name: " + v.name);
+
+		}
+	}
+}
+
+
+
