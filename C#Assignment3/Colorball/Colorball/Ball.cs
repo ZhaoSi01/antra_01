5f6b72ac9b880ea6bc98d2105e695fccb74626fe
Yang Yu <yy4252gg@gmail.com>
2022-07-01 00:00:55 -0400

    finished
    finished

--- C#Assignment3/Colorball/Colorball/Ball.cs
+++ C#Assignment3/Colorball/Colorball/Ball.cs
@@ -2,8 +2,24 @@
 
 namespace Colorball
 {
-	public class Ball
+	public class Ball : Color
 	{
+        public static void Main(string[] args)
+        {
+			var b1 = new Ball();
+			b1.ColorGet(0, 12, 0, 0);
+			b1.r = 3;
+			b1.thr();
+			b1.showR();
+
+			var b2 = new Ball();
+			b2.ColorGet(10, 12, 10, 0);
+			b2.r = 3;
+			b2.thr();
+			b2.showR();
+
+
+		}
 		//unsigned_char color;
 		//0 - 255
 		//byte color;
@@ -19,11 +35,31 @@ namespace Colorball
 		//	return j;
 		//}
 		//byte color { get; set; }
-		byte color { get; set; }
-		byte color { get; set; }
 
-		byte alpha { get; set; }
-		byte scale { get; set; }
+		int r { set; get; }
+
+		public void pop()
+		{
+			r = 0;
+            Console.WriteLine("Pop successfully");
+		}
+
+		public void thr(){
+			if(r != 0)
+            {
+				r++;
+				Console.WriteLine("Throw successfully");
+			}
+		}
+
+		public int showR()
+		{
+			Console.WriteLine("radius is : {0} cm", r);
+			return r;
+		}
+
+
+
 
 
 	}
--- C#Assignment3/Colorball/Colorball/Color.cs
+++ C#Assignment3/Colorball/Colorball/Color.cs
@@ -3,8 +3,21 @@ namespace Colorball
 {
 	public class Color
 	{
-		public Color()
+		byte red { get; set; }
+		byte blue { get; set; }
+		byte green { get; set; }
+		byte alpha { get; set; }
+		byte scale { get; set; }
+
+		
+		public byte ColorGet(byte Red, byte Blue, byte Green, byte Alpha)
 		{
+			red = Red;
+			blue = Blue;
+			green = Green;
+			alpha = Alpha;
+            Console.WriteLine("Color is {0}", (byte)((red + Blue + green) / 3));
+			return (byte)((red + Blue + green )/ 3);
 		}
 	}
 }
