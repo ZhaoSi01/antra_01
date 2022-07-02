574e849edc89523e99709e2a64fcf9ed69905921
Yang Yu <yy4252gg@gmail.com>
2022-07-01 23:36:01 -0400

    Generic Repository

---
+++ C#Assignment4/Repository/Repository/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+//For sql server
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace Repository
+{
+	public interface IRepository<T> where T : class
+    {
+        int Add(T obj);
+        void Remove(T obj);
+        void save();
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+
+    }
+
+
+
+
+}
+
+
+
---
+++ C#Assignment4/Repository/Repository/IdIRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository;
+
+namespace Repository.IdIRepository
+
+{
+    internal class IdIRepository : IRepository<Student>
+    {
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+        }
+
+        List<Student> idlist= new List<Student>();
+
+        public void Add(Student obj)
+        {
+            idlist.Add(obj);
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            return idlist;
+        }
+
+        //public Student GetById(int id)
+        //{
+        //    if (idlist.Contains(Student.Id))
+        //}
+
+        public void Remove(Student obj)
+        {
+            idlist.Remove(obj);
+        }
+
+        public void save()
+        {
+            //No operation
+            return;
+        }
+    }
+}
+
