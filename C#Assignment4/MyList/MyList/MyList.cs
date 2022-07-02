ac21c1705d3a29eced893eebbc41a0fdf2c0fc5b
Yang Yu <yy4252gg@gmail.com>
2022-07-01 22:47:20 -0400

    Generic List

--- C#Assignment4/MyList/MyList/MyList.cs
+++ C#Assignment4/MyList/MyList/MyList.cs
@@ -1,11 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;　
+
 namespace MyList
 {
-	public class MyList
+	public class MyList<T>
 	{
-		public MyList()
+
+        List<T> list = new List<T>();
+		
+
+
+		public static void Add(T element, List<T> list)
 		{
+            list.Add(element);
+            Console.WriteLine("Add {0} Successfully", element);
 		}
+
+		public T Remove(int index)
+		{
+			T tmp = list[index]; ;
+			list.RemoveAt(index);
+			return tmp;
+		}
+
+		public bool Contains(T element)
+        {
+			return list.Contains(element);
+        }
+
+		public void Clear()
+        {
+			list.Clear();
+
+        }
+
+		public void InsertAt(T ele, int index)
+		{
+			list.Insert(index, ele);
+
+		}
+
+		public void DeleteAt(int index)
+		{
+			//return type is void not T
+			//list.Remove(index);
+			list.RemoveAt(index);
+		}
+
+
+		public T find(int index)
+		{
+			return list[index];
+		}
+
+		
 	}
 }
 
---
+++ C#Assignment4/MyList/MyList/test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyList;
+namespace MyList
+{
+	public class test 
+	{
+		public static void Main(string[] args)
+		{
+
+			//List<int> t = new Mylist();
+			//t.Add(3,t);
+		}
+	}
+}
+
